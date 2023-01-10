8f16ab4ca74d7a24a61f7a177b31c05ef396f1bf
Vera <veraverous@gmail.com>
2023-01-10 19:54:16 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fact
+{
+    internal class Program
+    {
+        public static long Fact(long n)
+        {
+            if (n == 0)
+                return 1;
+            else
+                return n * Fact(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            int num  = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(Fact(num));
+        }
+    }
+}
