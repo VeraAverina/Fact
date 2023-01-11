dc96bcdf0522a8099bf59694fdac4e086e899be9
Vera <veraverous@gmail.com>
2023-01-11 20:08:33 +0300

    MStest

--- TestMSTest/UnitTest1.cs
+++ TestMSTest/UnitTest1.cs
@@ -1,27 +1,22 @@
-namespace TestFact
+namespace TestMSTest
 {
-    public class Tests
+    [TestClass]
+    public class UnitTest1
     {
-        [SetUp]
-        public void Setup()
+        [TestMethod]
+        public void TestMethod1()
         {
+            long f0 = Fact.Program.Fact(0);
+            Assert.AreEqual(1L,f0);
         }
-
-        [Test]
-        public void Test1()
-        {
-            long f0 = Fact. Program.Fact(0);
-            Assert.That(1L, Is.EqualTo(f0));
-        }
-
-        [Test]
+        [TestMethod]
         public void Test2()
         {
             long f0 = Fact.Program.Fact(5);
-            Assert.That(120L, Is.EqualTo(f0));
+            Assert.AreEqual(120L, f0);
         }
 
-        [Test]
+        [TestMethod]
         public void Test3()
         {
             Exception exc = null;
@@ -29,7 +24,8 @@ namespace TestFact
             {
                 long f0 = Fact.Program.Fact(-1);
             }
-            catch (Exception ex){
+            catch (Exception ex)
+            {
                 exc = ex;
             }
             Assert.IsNotNull(exc);
